09ac7831cd8a3c79bbd9085842665563a9936344
Akash Das <akashakki1100@gmail.com>
2025-10-27 16:22:37 +0530

    third

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -4,8 +4,8 @@ const MarkdownEditor = ({input, setInput}) => {
   return (
     <>
         <div className='textarea'>
-            <textarea style={{width: '50vw', height: '50vh'}} onChange={(e)=>setInput(e.target.value)} >
-                {input}
+            <textarea style={{width: '50vw', height: '50vh', fontWeight: "bold" }} onChange={(e)=>setInput(e.target.value)} >
+                <b>{input}</b>
             </textarea>
         </div>
 
